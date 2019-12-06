f9b45259e753413bd0e21b660272bfcb68edc7cd
Akshay <akshay.pramod@meltwater.com>
2019-12-06 16:59:48 +0530

    Update

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/CAW'),
+      dir: require('path').join(__dirname, './coverage/Akshay'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
